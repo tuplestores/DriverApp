e4c3294a5c28eb16ee62c772aff3a31c92f23e80
ajishd <ajishd.tuplestores@gmail.com>
2019-06-20 22:42:23 +0530

    commited changes by Ajish on Thursday 12:23

---
+++ app/src/main/java/com/tuplestores/driverapp/DriverHomeNotifyActivity.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019. TUPLE STORES .All Rights Reserved
+ */
+
+package com.tuplestores.driverapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverHomeNotifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_app_home_notify_drw_main);
+    }
+}
---
+++ app/src/main/java/com/tuplestores/driverapp/driver_home_notify.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019. TUPLE STORES .All Rights Reserved
+ */
+
+package com.tuplestores.driverapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class driver_home_notify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_home_notify);
+    }
+}
